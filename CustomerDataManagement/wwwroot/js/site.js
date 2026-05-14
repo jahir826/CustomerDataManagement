6770859a81bb678850f23a2f0854ca72dc346ca5
jahir826 <USER@DESKTOP-VDQB6AF>
2026-05-14 11:56:12 +0530

    changes some forgotPassword pages

--- CustomerDataManagement/wwwroot/js/site.js
+++ CustomerDataManagement/wwwroot/js/site.js
@@ -1,15 +1,1 @@
-﻿// <script>
-//     function openEditModal(cid, name, city, balance, status) {
-
-//         Data fill karo
-//         document.getElementById("Custid").value = cid;
-//     document.getElementById("Name").value = name;
-//     document.getElementById("City").value = city;
-//     document.getElementById("Balance").value = balance;
-//     document.getElementById("Status").value = status;
-
-//     Modal open karo
-//     var myModal = new bootstrap.Modal(document.getElementById('editModal'));
-//     myModal.show();
-//     }
-// </script>
+﻿
